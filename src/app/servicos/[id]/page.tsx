3c492f884dbf9a666eff50d4d0c255dd10be0fdc
Seu Nome <seu@exemplo.com>
2026-05-19 15:35:51 -0300

    3.0.4

--- src/app/servicos/[id]/page.tsx
+++ src/app/servicos/[id]/page.tsx
@@ -108,7 +108,7 @@ export default function ServicosPage(){
 
                             )}
                                 <RenderComments
-                                    comments={data?.comentarios}
+                                    comments={data?.comentarios || []}
                                 />
                         </div>
 
