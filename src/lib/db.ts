2942f6be2067da14d4dc75161bf50cf94c20dcce
Seu Nome <seu@exemplo.com>
2026-05-22 09:36:17 -0300

    0.13

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,16 +1,15 @@
-import { Authorizations } from "@/services/entitys/auhorizations/EntityAuthorization";
-import { AutoUser } from "@/services/entitys/auhorizations/EntityAutoUser";
-import { Coments } from "@/services/entitys/coments/EntityComents";
-import { NComentsUser } from "@/services/entitys/coments/EntityNComentsUser";
-import { AvaliationServices } from "@/services/entitys/PetServices/EntityAvaliation";
-import { DataService } from "@/services/entitys/PetServices/EntityDataService";
-import { NServicosDataHorario } from "@/services/entitys/PetServices/EntityNServicosData";
-import { Services } from "@/services/entitys/PetServices/EntityServices";
-import { EndressUser } from "@/services/entitys/User/EntityEnderecoUser";
-import { User } from "@/services/entitys/User/EntityUser";
-import { UserNumber } from "@/services/entitys/User/EntityUserNumber";
-import { UsuarioServicos } from "@/services/entitys/User/EntityUserServices";
-import "reflect-metadata";
+import { Authorizations } from "../services/entitys/auhorizations/EntityAuthorization";
+import { AutoUser } from "../services/entitys/auhorizations/EntityAutoUser";
+import { Coments } from "../services/entitys/coments/EntityComents";
+import { NComentsUser } from "../services/entitys/coments/EntityNComentsUser";
+import { AvaliationServices } from "../services/entitys/PetServices/EntityAvaliation";
+import { DataService } from "../services/entitys/PetServices/EntityDataService";
+import { NServicosDataHorario } from "../services/entitys/PetServices/EntityNServicosData";
+import { Services } from "../services/entitys/PetServices/EntityServices";
+import { EndressUser } from "../services/entitys/User/EntityEnderecoUser";
+import { User } from "../services/entitys/User/EntityUser";
+import { UserNumber } from "../services/entitys/User/EntityUserNumber";
+import { UsuarioServicos } from "../services/entitys/User/EntityUserServices";
 import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
