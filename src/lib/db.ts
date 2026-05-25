66f850d74fb97f2f833c5237cb6188a761e172c5
Seu Nome <seu@exemplo.com>
2026-05-25 09:32:44 -0300

    0.1

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,18 @@
 
 import { DataSource } from "typeorm";
 import path from "path";
+import { Authorizations } from "@/services/entitys/auhorizations/EntityAuthorization";
+import { AutoUser } from "@/services/entitys/auhorizations/EntityAutoUser";
+import { Coments } from "@/services/entitys/coments/EntityComents";
+import { NComentsUser } from "@/services/entitys/coments/EntityNComentsUser";
+import { AvaliationServices } from "@/services/entitys/PetServices/EntityAvaliation";
+import { DataService } from "@/services/entitys/PetServices/EntityDataService";
+import { NServicosDataHorario } from "@/services/entitys/PetServices/EntityNServicosData";
+import { Services } from "@/services/entitys/PetServices/EntityServices";
+import { EndressUser } from "@/services/entitys/User/EntityEnderecoUser";
+import { User } from "@/services/entitys/User/EntityUser";
+import { UserNumber } from "@/services/entitys/User/EntityUserNumber";
+import { UsuarioServicos } from "@/services/entitys/User/EntityUserServices";
 
 export const AppDataSource = new DataSource({
     type: "postgres", 
@@ -11,7 +23,18 @@ export const AppDataSource = new DataSource({
     synchronize: false,
     logging: false,
     entities: [
-        path.join(__dirname, "../services/entitys/**/*.{ts,js}")
+        Authorizations,
+        AutoUser,
+        Coments,
+        NComentsUser,
+        AvaliationServices,
+        DataService,
+        NServicosDataHorario,
+        Services,
+        EndressUser,
+        User,
+        UserNumber,
+        UsuarioServicos
     ]
 });
 
