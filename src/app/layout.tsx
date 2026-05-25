ed2a7a298eaaa9fbe65b5cc8a83d021b40deda31
Seu Nome <seu@exemplo.com>
2026-05-25 12:06:52 -0300

    0.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,8 @@
-
 import './global.css'
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { GlobalProvider } from "@/lib/GlobalContext";
 
 export const dynamic = 'force-dynamic';
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "CrPet",
@@ -28,9 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Geist:wght@100..900&family=Geist+Mono:wght@100..900&display=swap" rel="stylesheet" />
+      </head>
+      <body className="antialiased">
         <GlobalProvider>
           {children}
         </GlobalProvider>
