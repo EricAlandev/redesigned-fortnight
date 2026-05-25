4f002781c116f3d491987bf13cd2c94dab62b03b
Seu Nome <seu@exemplo.com>
2026-05-25 16:26:46 -0300

    0.1

--- next.config.ts
+++ next.config.ts
@@ -2,23 +2,22 @@
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  webpack: (config) => {
-    config.external = [...(config.externals || []), 'canvas', 'jsdom'];
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      fs: false,
-      net: false,
-      tls: false,
-      child_process: false,
-      "react-native-sqlite-storage": false,
-      "@sap/hana-client": false,
-      "hdb-pool": false,
-      "antlr4ts": false,
-      "mysql2": false,
-      "oracledb": false,
-      "pg-native": false,
-    };
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // This forces Webpack to ignore the drivers that don't exist in Vercel
+      config.externals.push({
+        'react-native-sqlite-storage': 'react-native-sqlite-storage',
+        'sqlite3': 'sqlite3',
+        'mysql2': 'mysql2',
+        'oracledb': 'oracledb',
+        'pg-query-stream': 'pg-query-stream',
+        '@sap/hana-client': '@sap/hana-client',
+        'hdb-pool': 'hdb-pool',
+        'antlr4ts': 'antlr4ts',
+      });
+    }
     return config;
   },
 };
+
 export default nextConfig;
