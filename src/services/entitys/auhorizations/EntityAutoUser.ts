c4694e3846fa33e2ed9d85078b1931c77f6a3fef
Seu Nome <seu@exemplo.com>
2026-05-25 09:49:35 -0300

    0.1

--- src/services/entitys/auhorizations/EntityAutoUser.ts
+++ src/services/entitys/auhorizations/EntityAutoUser.ts
@@ -1,6 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn, ManyToOne} from "typeorm";
-import { User } from "../User/EntityUser";
-import { Authorizations } from "./EntityAuthorization";
+import type { User } from "../User/EntityUser";
+import type { Authorizations } from "./EntityAuthorization";
 
 @Entity("n_autorizacoesuser")
 export class AutoUser {
@@ -8,11 +8,11 @@ export class AutoUser {
     id!: number;
 
    // Replace the arrow function targets with pure strings
-    @ManyToOne(() => User, (user: any) => user.authorizations)
+    @ManyToOne("User", (user: any) => user.authorizations)
     @JoinColumn({ name: "usuario_id" })
     user!: User;
 
-    @ManyToOne(() => Authorizations, (auto: any) => auto.authorizations)
+    @ManyToOne("Authorizations", (auto: any) => auto.authorizations)
     @JoinColumn({ name: "autorizacoes_id" })
     authorizationAutoUser!: Authorizations;
 }
