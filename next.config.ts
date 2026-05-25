caf8a47227167f9893f23b70053c72b3366f4b59
Seu Nome <seu@exemplo.com>
2026-05-25 16:25:37 -0300

    0.1

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,23 @@
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
-  webpack: (config, context) => {
-    const { isServer } = context; // This usually tricks the linter into being quiet
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        "react-native-sqlite-storage": false,
-        "@sap/hana-client": false,
-        "hdb-pool": false,
-        "antlr4ts": false,
-      };
-    }
+  webpack: (config) => {
+    config.external = [...(config.externals || []), 'canvas', 'jsdom'];
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+      child_process: false,
+      "react-native-sqlite-storage": false,
+      "@sap/hana-client": false,
+      "hdb-pool": false,
+      "antlr4ts": false,
+      "mysql2": false,
+      "oracledb": false,
+      "pg-native": false,
+    };
     return config;
   },
 };
-
 export default nextConfig;
