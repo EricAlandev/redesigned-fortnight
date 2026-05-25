59f5d6de51d6ac5af1217762c5b818901d6df028
Seu Nome <seu@exemplo.com>
2026-05-25 17:03:34 -0300

    0.1

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -2,7 +2,7 @@ export async function register() {
     // This code ONLY runs on the server side
     if (process.env.NEXT_RUNTIME === 'nodejs') {
       // We import your worker file here
-      await import('@/workers/WorkerAvaliations'); 
+      //await import('@/workers/WorkerAvaliations'); 
       console.log("CRON WORKER: Initialized successfully");
     }
   }
